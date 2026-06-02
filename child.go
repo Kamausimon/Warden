68c84d32d2d2e9ffdd5609ef8e341bd3ac131399
kamausimon <kamausimon217@gmail.com>
2026-06-02 06:19:05 +0300

    Enhance child function to implement root filesystem pivoting and namespace isolation

--- child.go
+++ child.go
@@ -3,12 +3,56 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"syscall"
 )
 
 func child() {
-	syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
-	syscall.Mount("proc", "/proc", "proc", 0, "")
+	newroot := "/tmp/warden-rootfs"
+
+	err := syscall.Mount("", "/", "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+	if err != nil {
+		fmt.Printf("child: error making namespace private: %v\n", err)
+		return
+	}
+
+	err = syscall.Mount(newroot, newroot, "", syscall.MS_BIND|syscall.MS_REC, "")
+	if err != nil {
+		fmt.Printf("child: error bind-mounting root filesystem: %v\n", err)
+		return
+	}
+
+	err = syscall.Mount("", newroot, "", syscall.MS_PRIVATE|syscall.MS_REC, "")
+	if err != nil {
+		fmt.Printf("child: error making mount private: %v\n", err)
+		return
+	}
+	oldRootput := filepath.Join(newroot, ".old_root")
+	if err := os.MkdirAll(oldRootput, 0700); err != nil {
+		fmt.Printf("child: error creating old root directory: %v\n", err)
+		return
+	}
+
+	if err := syscall.PivotRoot(newroot, oldRootput); err != nil {
+		fmt.Printf("child: error pivoting root filesystem: %v\n", err)
+		return
+	}
+
+	if err := syscall.Chdir("/"); err != nil {
+		fmt.Printf("child: error changing working directory: %v\n", err)
+		return
+	}
+	if err := syscall.Unmount("/.old_root", syscall.MNT_DETACH); err != nil {
+		fmt.Printf("child: error unmounting old root filesystem: %v\n", err)
+		return
+	}
+	os.RemoveAll("/.old_root")
+
+	err = syscall.Mount("proc", "/proc", "proc", 0, "")
+	if err != nil {
+		fmt.Printf("child: error mounting proc filesystem: %v\n", err)
+		return
+	}
 	syscall.Sethostname([]byte("container"))
 
 	fmt.Printf("The PID of the current running child %v", os.Getpid())
